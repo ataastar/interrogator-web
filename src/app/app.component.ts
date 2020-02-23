846108da0e5b976a3273a53a0e1bc971f22784dd
ataastar <ata.astar@gmail.com>
2020-02-23 11:23:42 +0100

    google oauth button part 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 import '../assets/css/styles.css';
+import * as queryString from 'query-string';
 
 @Component({
     selector: 'learn-english-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {
+
+    stringifiedParams = queryString.stringify({
+        client_id: '423949024920-kl1emj9rbq5753ht3h6qqrlub0u054pq.apps.googleusercontent.com',
+        redirect_uri: 'http://localhost/authenticate/google',
+        scope: [
+          'https://www.googleapis.com/auth/userinfo.email',
+        ].join(' '), // space seperated string
+        response_type: 'code',
+        access_type: 'offline',
+        prompt: 'consent',
+      });
+      
+      googleLoginUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' + this.stringifiedParams;
+}

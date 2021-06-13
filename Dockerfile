1ec899579bd5f636d753a9b4919b29aed36d5148
ataastar <ata.astar@gmail.com>
2021-06-13 21:16:56 +0200

    qovery env file added

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN npm install
 
 COPY . /app
 
-RUN npm run build --prod
+RUN npm run build --qovery
 
 # Stage 2
 
---
+++ src/environments/environment.qovery.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://main-interro-ntwgdm36gu76t87v-gtw.qovery.io'
+};

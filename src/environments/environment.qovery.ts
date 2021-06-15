bd3cf1315dcb75b7492434eafd7be27c57276e40
ataastar <ata.astar@gmail.com>
2021-06-15 19:47:18 +0200

    use api on master

--- src/environments/environment.qovery.ts
+++ src/environments/environment.qovery.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://main-interro-ntwgdm36gu76t87v-gtw.qovery.io'
+  apiUrl: 'https://main-interro-tl8ch045ag2h1cfx-gtw.qovery.io'
 };

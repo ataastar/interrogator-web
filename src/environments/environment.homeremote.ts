d074443c30172bd02130a75bdc9a7aae7bce3404
ataastar <ata.astar@gmail.com>
2021-10-09 14:32:19 +0200

    subdomain changed

--- src/environments/environment.homeremote.ts
+++ src/environments/environment.homeremote.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://interrogator.mooo.com:3000'
-  apiUrl: 'http://huneng.mooo.com:3000'
+  apiUrl: 'http://interrogator.mooo.com:3000'
 };

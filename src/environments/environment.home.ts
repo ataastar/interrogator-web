4b5bf0fdf148a2f3865c4800ae06723e5163fd04
ataastar <ata.astar@gmail.com>
2021-11-13 16:12:57 +0100

    home environment

--- src/environments/environment.home.ts
+++ src/environments/environment.home.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.81:3000'
+  apiUrl: 'http://192.168.0.213:3000' // lan
+  //apiUrl: 'http://192.168.0.81:3000' // wifi
 };

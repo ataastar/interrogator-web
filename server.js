c77ae2e5737a153ee371b848d293fc0826fa87a9
ataastar <ata.astar@gmail.com>
2022-02-04 20:19:48 +0100

    express is installed, dist->build

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist'));
+app.use(express.static('./build'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/'}),
+  res.sendFile('index.html', {root: 'build/'}),
 );
 
 app.listen(process.env.PORT || 8080);

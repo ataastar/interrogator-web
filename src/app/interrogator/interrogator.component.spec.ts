f6da307a18f3e7dd8bbe36731f23a1d70918c4d2
Molnár Attila <a.molnar@mortoff.hu>
2023-01-03 16:57:56 +0100

    unit test the interrogation workflow, spy the getRandom function

--- src/app/interrogator/interrogator.component.spec.ts
+++ src/app/interrogator/interrogator.component.spec.ts
@@ -12,6 +12,10 @@ function setupWordService() {
   return wordServiceSpy;
 }
 
+function setupMockWordService() {
+  return jasmine.createSpyObj('WordService', ['wrongAnswer', 'rightAnswer']);
+}
+
 function setup(wordService?): InterrogatorComponent {
   return new InterrogatorComponent(wordService, null, null);
 }
@@ -69,7 +73,7 @@ describe('InterrogatorComponent', () => {
   });
 
   it('Interrogation workflow with 1 word. 1 Right answer', () => {
-    const component = setup();
+    const component = setup(setupMockWordService());
 
     expect(component.fillWordArrays()).toBeFalsy();
 
@@ -85,11 +89,92 @@ describe('InterrogatorComponent', () => {
     expect(component.currentWordArray.length).toBe(1);
     expect(component.actualWords.length).toBe(1);
 
+    component.next();
+    component.to = 'b1';
+    component.check();
+    expect(component.checked).toBeTruthy('to be checked');
+    expect(component.wrong).toBeFalsy('the answer right, so can\'t be wrong');
+    expect(component.actualWords.length).toBe(0, 'Can not be any word to interrogate');
+
+  });
+
+  it('Interrogation workflow with 1 word. 1 wrong answer, 1 right answer', () => {
+    const component = setup(setupMockWordService());
+
+    expect(component.fillWordArrays()).toBeFalsy();
+
+    const words: GuessedWord[] = []
+    let _1MinuteAgo = new Date(new Date().getTime() - 1000 * 60);
+    words.push(createGuessedWord(_1MinuteAgo, _1MinuteAgo, 'a1', 'b1'));
+    const a = component.categorizeWords(words, true);
+    expect(a.length).toEqual(1);
+    expect(a[0].length).toEqual(1);
+
+    expect(component.fillWordArrays()).toBeTruthy();
+    expect(component.categorizedWords.length).toBe(0);
+    expect(component.currentWordArray.length).toBe(1);
+    expect(component.actualWords.length).toBe(1);
+
+    // wrong answer
+    component.next();
+    component.to = 'c3';
+    component.check();
+    expect(component.checked).toBeTruthy('to be checked');
+    expect(component.wrong).toBeTruthy('the answer was wrong');
+    expect(component.actualWords.length).toBe(1, 'the 1 word is should be interrogated');
+
+    // right answer
+    component.next();
+    component.to = 'b1';
+    component.check();
+    expect(component.checked).toBeTruthy('to be checked');
+    expect(component.wrong).toBeFalsy('the answer right, so can\'t be wrong');
+    expect(component.actualWords.length).toBe(0, 'Can not be any word to interrogate');
+
+  });
+
+  it('Interrogation workflow with 2 words. 2 right answers', () => {
+    const component = setup(setupMockWordService());
+
+    expect(component.fillWordArrays()).toBeFalsy();
+
+    const words: GuessedWord[] = []
+    let _1MinuteAgo = new Date(new Date().getTime() - 1000 * 60);
+    const word1 = createGuessedWord(_1MinuteAgo, _1MinuteAgo, 'a1', 'b1')
+    words.push(word1);
+    const word2 = createGuessedWord(_1MinuteAgo, _1MinuteAgo, 'aa1', 'bb1');
+    words.push(word2);
+    const a = component.categorizeWords(words, true);
+    expect(a.length).toEqual(1);
+    expect(a[0].length).toEqual(2);
+
+    expect(component.fillWordArrays()).toBeTruthy();
+    expect(component.categorizedWords.length).toBe(0);
+    expect(component.currentWordArray.length).toBe(2);
+    expect(component.actualWords.length).toBe(2);
+
+    // right answer
+    spyOn(component, 'getRandomWord').and.returnValue(word1);
+    component.next();
+    component.to = 'b1';
+    component.check();
+    expect(component.checked).toBeTruthy('to be checked');
+    expect(component.wrong).toBeFalsy('the answer right, so can\'t be wrong');
+    expect(component.actualWords.length).toBe(1, 'aa1 is remained in the array');
+    expect(component.actualWords[0].word.from[0].phrase).toBe('aa1', 'aa1 is remained in the array');
+
+    // right answer
+    component.getRandomWord = jasmine.createSpy().and.returnValue(word2);
+    component.next();
+    component.to = 'bb1';
+    component.check();
+    expect(component.checked).toBeTruthy('to be checked');
+    expect(component.wrong).toBeFalsy('the answer right, so can\'t be wrong');
+    expect(component.actualWords.length).toBe(0, 'Can not be any word to interrogate');
 
   });
 });
 
 function createGuessedWord(nextInterrogationTime: Date, lastAnswerTime: Date, fromWord?: string, toWord?: string): GuessedWord {
   return new GuessedWord(new Word(null, [new Phrase(fromWord, 1)], [new Phrase(toWord, 2)], null, null, nextInterrogationTime.toUTCString(), lastAnswerTime.toUTCString()));
-
 }
